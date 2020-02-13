ef361927dd0c588a6710c39b6c3ee27bc3d09d65
Arsene <arsenepoutsi@gmail.com>
2020-02-13 15:49:29 +0100

    Mise a jour du site web Madera

---
+++ templates/footer.php
@@ -0,0 +1,47 @@
+<div class="footer bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <p class="mb-4"><img src="images/logo.png" alt="Image" class="img-fluid"></p>
+        <p style="text-align:justify">Le concept MADERA est orienté vers des solutions modulaires innovantes et offre des réalisations uniques de bâtiments architecturés en bois.</p>  
+        <p><a href="#">Lire la suite</a></p>
+      </div>
+      <div class="col-lg-3">
+        <h3 class="footer-heading"><span>Madera construction</span></h3>
+        <ul class="list-unstyled">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">L'Entreprise</a></li>
+            <li><a href="#">Nos Solutions</a></li>
+            <li><a href="#">CCTP</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3">
+          <h3 class="footer-heading"><span>Nos Solutions</span></h3>
+          <ul class="list-unstyled">
+              <li><a href="#">Bureaux</a></li>
+              <li><a href="#">Bureaux d'ateliers</a></li>
+              <li><a href="#">Locaux sociaux</a></li>
+              <li><a href="#">Locaux divers</a></li>
+          </ul>
+      </div>
+      <div class="col-lg-3">
+          <h3 class="footer-heading"><span>Contact</span></h3>
+          <ul class="list-unstyled">
+          <li><a href="#"><b>Tél. :</b>&nbsp;&nbsp;<span>02 51 98 18 32</span></a></li>
+              <li><a href="#"><b>E-mail :</b>&nbsp;&nbsp;<span>contact@madera-construction.com</span></a></li>
+          </ul>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <div class="copyright">
+            <p>
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits réservés | Madera construction par <a href="http://www.ab-labsolutions.com" target="_blank" >AB-LABSOLUTIONS</a>
+            </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ templates/header.php
@@ -0,0 +1,122 @@
+<div class="site-mobile-menu site-navbar-target">
+  <div class="site-mobile-menu-header">
+    <div class="site-mobile-menu-close mt-3">
+      <span class="icon-close2 js-menu-toggle"></span>
+    </div>
+  </div>
+  <div class="site-mobile-menu-body"></div>
+</div>
+
+<div class="header-top bg-light">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-6 col-lg-3">
+        <a href="index.html">
+          <img src="images/logo.png" alt="Image" class="img-fluid">
+        </a>
+      </div>
+      <div class="col-lg-3 d-none d-lg-block">
+
+        <div class="quick-contact-icons d-flex">
+          <div class="icon align-self-start" style="background:#a2c037;">
+            <span class="icon-location-arrow text-primary"></span>
+          </div>
+          <div class="text">
+            <span class="h4 d-block">La Roche sur Yon</span>
+            <span class="caption-text">Acti Est - ZA Les ajoncs</span>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-lg-3 d-none d-lg-block">
+        <div class="quick-contact-icons d-flex">
+          <div class="icon align-self-start" style="background:#a2c037;">
+            <span class="icon-phone text-primary"></span>
+          </div>
+          <div class="text">
+            <span class="h4 d-block">02 51 98 18 32</span>
+            <span class="caption-text">Appel Gratuit</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 d-none d-lg-block">
+        <div class="quick-contact-icons d-flex">
+          <div class="icon align-self-start" style="background:#a2c037;">
+            <span class="icon-envelope text-primary"></span>
+          </div>
+          <div class="text">
+            <span class="h4 d-block">contact@madera-construction.com</span>
+            <span class="caption-text">Madrea construction</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 d-block d-lg-none text-right">
+          <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+            class="icon-menu h3"></span></a>
+      </div>
+    </div>
+  </div>
+   
+  <div class="site-navbar py-2 js-sticky-header site-navbar-target d-none pl-0 d-lg-block" role="banner">
+
+  <div class="container">
+    <div class="d-flex align-items-center">
+      
+      <div class="mx-auto">
+        <nav class="site-navigation position-relative text-right" role="navigation">
+          <ul class="site-menu main-menu js-clone-nav mr-auto d-none pl-0 d-lg-block">
+            <li class="active">
+              <a href="index.html" class="nav-link text-left">Accueil</a>
+            </li>
+            <li>
+              <a href="about.html" class="nav-link text-left">L'Entreprise</a>
+            </li>
+            <li>
+              <a href="services.html" class="nav-link text-left">Nos Solutions</a>
+            </li>
+            <li>
+                <a href="testimonials.html" class="nav-link text-left">CCTP</a>
+            </li>
+            <li>
+                <a href="contact.html" class="nav-link text-left">Contact</a>
+              </li>
+          </ul> 
+        </nav>
+
+      </div>
+     
+    </div>
+  </div>
+
+</div>
+
+</div>
+
+<div class="hero-slide owl-carousel site-blocks-cover">
+  <div class="intro-section" style="background-image: url('images/slide2.jpg');">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-7 ml-auto text-right" data-aos="fade-up">
+          <h1>Le bois et le design</h1>
+          <p>au service d'une modularité économe.</p>
+          <p><a href="#" class="btn btn-primary py-3 px-5" style="background:#a2c037;border-color:#a2c037;">Lire la suite</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="intro-section" style="background-image: url('images/slide1.jpg');">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-7 mx-auto text-center" data-aos="fade-up">
+        <h1>Le bois et le design</h1>
+          <p>au service d'une modularité économe.</p>
+          <p><a href="#" class="btn btn-primary py-3 px-5" style="background:#a2c037;border-color:#a2c037;">Lire la suite</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
